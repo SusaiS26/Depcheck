7138bc1f0304da9645cf5805637e18a4b890d167
SusaiS26 <susaimm26@gmail.com>
2024-04-24 13:22:13 +0530

    input

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
 
 function App() {
+  const [newvalues, setNewvalues] = useState("")
+
+  const onchangevalnew = (e) => {
+    setNewvalues(e.target.value)
+  }
+
   return (
     <div>
       <h1>Susai Nathan</h1>
+      {newvalues}
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
@@ -18,6 +26,8 @@ function App() {
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
       </Form>
+
+      <input type="text" value={newvalues} onChange={onchangevalnew}></input>
       <Button variant="primary">Primary1</Button>
       <Button variant="secondary">Secondary2</Button>
     </div>
