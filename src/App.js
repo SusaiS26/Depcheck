29d5de66ee47a7c5b0b39a8016d03accdf80b61c
SusaiS26 <susaimm26@gmail.com>
2024-04-24 13:12:54 +0530

    boostrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Newpage from './Newpage';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path='/New' element={<Newpage />}></Route>
-        </Routes>
-      </Router>
+      <h1>Susai Nathan</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+      </Form>
+      <Button variant="primary">Primary1</Button>
+      <Button variant="secondary">Secondary2</Button>
     </div>
   );
 }
